0f412778baf41e62391d6124b41574e04e59563b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-21 15:45:40 -0500

    add render prop to Toggle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './Toggle';
-
+import Toggle from './ToggleRenderProps';
 class App extends Component {
   render() {
     return (
@@ -11,9 +10,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Toggle>
-          <h1>Test</h1>
-        </Toggle>
+        <Toggle
+          render={text => (
+            <div>
+              <h1>{text}</h1>
+              <button>Show / Hide</button>
+            </div>
+          )}
+        />
       </div>
     );
   }
